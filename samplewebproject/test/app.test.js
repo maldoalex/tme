57b28e3d7252dbbb5fff15ae5a249b6976032188
Alex Maldonado <maldoalex@gmail.com>
2020-04-22 15:09:31 -0500

    Initial commit

---
+++ samplewebproject/test/app.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+
+it('has a text input', async () => {
+ const dom = await render('index.html');
+
+ const input = dom.window.document.querySelector('input');
+
+ assert(input);
+});
+
+it('shows a success message with a valid email', async () => {
+ const dom = await render('index.html');
+
+ const input = dom.window.document.querySelector('input');
+ input.value = 'aasdfasf@adfaff.com';
+ dom.window.document
+  .querySelector('form')
+  .dispatchEvent(new dom.window.Event('submit'));
+
+ const h1 = dom.window.document.querySelector('h1');
+
+ console.log(h1.innerHTML);
+});
